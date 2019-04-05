050a12e9c52d1a021713c2f27751e41bbd73e794
Robin <r.meier@computer-meier.de>
2019-04-05 13:02:33 +0200

    Better parser

--- DataPreparation.py
+++ DataPreparation.py
@@ -1,14 +1,21 @@
 bookStr = ''
+bodyStr = ''
 bookArray = []
 
 with open('Data/train_small.txt','r') as file:
 	for line in file:
 		if line.startswith('<book'):
 			bookStr = ''
+			bodyStr = ''
 			bookStr += line
 		elif line.startswith('</book>'):
 			bookStr += line
-			bookArray.append(bookStr)
+			bookArray.append((bookStr,bodyStr))
+		elif line.startswith('<body>'):
+			bookStr += line
+			bodyStr += line
+			bodyStr = bodyStr[:-7]
+			bodyStr = bodyStr[6:]
 		else:
 			bookStr += line
 
